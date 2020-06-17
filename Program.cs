f166c9a9ed30e345426d13e8b5682c00117bdaa3
connor <vbrookesy@hotmail.co.uk>
2020-06-17 11:09:58 +0100

    Added 3 new methods

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class UserInput
+    {
+        public void ShowMeUserInput(int userData)
+        {
+
+        }
+        public void ShowMeUserInput(string userData)
+        {
+            
+        }
+        public void ShowMeUserInput(double userData)
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
