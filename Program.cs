bd1850b11bd2b356269cdd9d405064422e7e8319
connor <vbrookesy@hotmail.co.uk>
2020-06-17 11:28:47 +0100

    Static polymorphism first eg

--- Program.cs
+++ Program.cs
@@ -6,22 +6,26 @@ namespace polymorphism
     {
         public void ShowMeUserInput(int userData)
         {
-
+            Console.WriteLine($"The user inputted an integer: {userData}");
         }
         public void ShowMeUserInput(string userData)
         {
-            
+            Console.WriteLine($"The user inputted a string: {userData}");
         }
-        public void ShowMeUserInput(double userData)
+        public void ShowMeUserInput(double userData, double anotherNumber)
         {
-            
+            Console.WriteLine($"The user inputted 2 doubles: {userData} & {anotherNumber} ");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserInput newUser = new UserInput();
+            newUser.ShowMeUserInput(123);
+            newUser.ShowMeUserInput("Connor");
+            newUser.ShowMeUserInput("Connor");
+            newUser.ShowMeUserInput(123.456, 22.3);
         }
     }
 }
